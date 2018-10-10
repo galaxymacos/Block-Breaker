bd1cdbcd1f08b05c7a57d5add85a67be826967df
galaxymacos <galaxymaxx@outlook.com>
2018-10-10 01:25:41 -0400

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int lives = 3;
+
+    [SerializeField] private MoveBall ball; // Script is a class in Unity
+    [SerializeField] private GameObject btnRetry;
+    [SerializeField] private GameObject btnExit;
+
+    private int point = 0;
+    [SerializeField] private Text score;
+
+    private int blocks = 0;
+    [SerializeField] private GameObject[] levels;
+    [SerializeField] private GameObject player;
+    private int currLvl = 0;
+    private GameObject currBoard;
+    [SerializeField] private float countDownTimer = 10;
+    [SerializeField] private string sceneToLoad;
+
+    private bool isBig = false;
+
+    private int bestScore = 0;
+
+    void LoadLvl()
+    {
+        if (currBoard)
+        {
+            Destroy(currBoard);
+        }
+
+        blocks = 0;
+        currBoard = Instantiate(levels[currLvl]);
+    }
+
+    public void AddBlock()
+    {
+        blocks++;
+    }
+
+    public void Death()
+    {
+        lives--;
+        if (lives > 0)
+        {
+            ball.Init();
+        }
+        else
+        {
+            ball.Init();
+            score.text = "Game over\nScore:" + point.ToString("D8");
+//            ball.gameObject.SetActive(false);
+            if (point > bestScore)
+            {
+                PlayerPrefs.SetInt("Score", point);
+                PlayerPrefs.Save();
+            }
+
+            btnRetry.SetActive(true);
+            btnExit.SetActive(true);
+        }
+    }
+
+    public void AddPoints()
+    {
+        point += 100;
+        string preText = "";
+        if (point > bestScore)
+        {
+            preText = "Best ";
+        }
+
+        score.text = preText + "Score:" + point.ToString("D8");
+        blocks--;
+        if (blocks <= 0)
+        {
+            if (currLvl < levels.Length - 1)
+            {
+                // We aren't at the last level
+                currLvl++;
+            }
+            else
+            {
+                // We are not at the last level
+                Debug.Log("Replay the last level");
+            }
+
+            ball.Init();
+            LoadLvl();
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        bestScore = PlayerPrefs.GetInt("Score", 0);
+        LoadLvl();
+        score.text = "Score: " + bestScore.ToString("D8"); // 8 decimals
+    }
+
+    public void GetPowerUp()
+    {
+        if (!isBig)
+        {
+            var paddleSprite = player.GetComponent<SpriteRenderer>();
+            var paddleCollider = player.GetComponent<BoxCollider2D>();
+            paddleCollider.size += new Vector2(paddleCollider.size.x, 0);
+            paddleSprite.size += new Vector2(paddleSprite.size.x, 0);
+            isBig = true;
+        }
+    }
+
+    public void ResetSize()
+    {
+        var paddle = player.GetComponent<SpriteRenderer>();
+        paddle.size -= new Vector2(paddle.size.x / 2, 0);
+        var paddleCollider = player.GetComponent<BoxCollider2D>();
+        paddleCollider.size += new Vector2(paddleCollider.size.x, 0);
+        isBig = false;
+        countDownTimer = 10f; // BUG restore timer to original
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBig)
+        {
+            countDownTimer -= Time.deltaTime;
+            if (countDownTimer <= 0.1f)
+            {
+                ResetSize();
+            }
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    
+    public void Retry()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+//        currLvl = 0;
+//        LoadLvl();
+//        ball.Init();
+//        lives = 3;
+//        btnExit.SetActive(false);
+//        btnRetry.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/BrickBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickBehaviour : MonoBehaviour
+{
+
+	[SerializeField] private GameObject powerUp;
+	[SerializeField] private int PowerUPPercentage = 20;
+	private GameObject ball;
+	
+	private GameManager code;
+	// Use this for initialization
+	void Start () {
+		code = GameObject.Find("GameManager").GetComponent<GameManager>();
+		code.AddBlock();
+		ball = GameObject.Find("ball");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionExit2D(Collision2D other)
+	{
+		float destiny = Random.Range(1, 100);
+		Debug.Log(destiny);
+		if (destiny > 0 && destiny <= PowerUPPercentage)
+		{
+			Instantiate(powerUp, ball.transform.position,Quaternion.identity);
+		}
+		
+
+		
+		Destroy(gameObject);
+		code.AddPoints();	
+	}
+
+	
+}
